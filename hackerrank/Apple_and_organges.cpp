cee2324c8482ba334ba33855704519d75722b0f3
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2021-04-04 22:43:16 +0530

    Update Apple_and_organges.cpp

--- hackerrank/Apple_and_organges.cpp
+++ hackerrank/Apple_and_organges.cpp
@@ -1,3 +1,10 @@
+int getCount(vector<int> v, int pos, int l, int r){
+    int count=0;
+    for(int i=0; i<v.size(); i++)
+       if(pos+v[i] >= l && pos+v[i] <= r)
+           count++;
+    return count;
+}
 void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges) {
       int count = getCount(apples, a, s, t);
       cout<<count<<"\n";

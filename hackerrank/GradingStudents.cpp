129ec67ebe37318cf64cc5177e05090744837798
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2021-04-04 22:45:55 +0530

    Delete GradingStudents.cpp

--- hackerrank/GradingStudents.cpp
+++
@@ -1,6 +0,0 @@
-vector<int> gradingStudents(vector<int> grades) {
-    for(int i=0; i<grades.size(); i++)
-        if(grades[i] >= 38 && grades[i]%5 > 2)
-            grades[i] += 5-grades[i]%5;
-     return grades;
-}

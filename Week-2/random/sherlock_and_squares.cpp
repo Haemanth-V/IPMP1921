cd5c500a51bd047656d95739ecd9762ef103602d
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2021-04-11 01:14:03 +0530

    Create sherlock_and_squares.cpp

---
+++ Week-2/random/sherlock_and_squares.cpp
@@ -0,0 +1,6 @@
+//Question link: https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+
+// Complete the squares function below.
+int squares(int a, int b) {
+    return floor(sqrt(b)) - ceil(sqrt(a)) + 1;
+}

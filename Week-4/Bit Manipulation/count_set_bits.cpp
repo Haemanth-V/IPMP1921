c82dd8573eb2db9f6bc895cacd20abe351de2a4d
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2021-04-19 15:59:12 +0530

    Create count_set_bits.cpp

---
+++ Week-4/Bit Manipulation/count_set_bits.cpp
@@ -0,0 +1,14 @@
+//Question link: https://leetcode.com/problems/number-of-1-bits/
+
+//Brian Kernighan's algorithm
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n){
+            n = n&(n-1);
+            count++;
+        }
+        return count;
+    }
+};

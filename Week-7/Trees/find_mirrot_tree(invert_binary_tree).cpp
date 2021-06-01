2c84919504c80a5950a1d187e2a1a82b2427ffbb
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2021-06-02 01:58:34 +0530

    Create find_mirrot_tree(invert_binary_tree).cpp

---
+++ Week-7/Trees/find_mirrot_tree(invert_binary_tree).cpp
@@ -0,0 +1,15 @@
+//Question link: https://leetcode.com/problems/invert-binary-tree/
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+            return NULL;
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode *t = root->left;
+        root->left = root->right;
+        root->right = t;
+        return root;
+    }
+};

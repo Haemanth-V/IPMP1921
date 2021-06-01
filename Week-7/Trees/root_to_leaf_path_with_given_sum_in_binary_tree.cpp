50d3338b7daaca74088b14fbf8b42df6960c5b78
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2021-06-02 01:48:56 +0530

    Create root_to_leaf_path_with_given_sum_in_binary_tree.cpp

---
+++ Week-7/Trees/root_to_leaf_path_with_given_sum_in_binary_tree.cpp
@@ -0,0 +1,12 @@
+//Question link: https://leetcode.com/problems/path-sum/
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root == NULL)
+            return false;
+        if(!root->left && !root->right && targetSum == root->val)
+            return true;
+        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+    }
+};

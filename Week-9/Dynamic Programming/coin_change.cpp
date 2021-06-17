dfcca0b4b070c27c2cc2f94e67f6ffdc32383624
Haemanth-V <59503851+Haemanth-V@users.noreply.github.com>
2021-06-18 02:18:04 +0530

    Create coin_change.cpp

---
+++ Week-9/Dynamic Programming/coin_change.cpp
@@ -0,0 +1,43 @@
+//Question link: 
+
+//Recursive 
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        return change(amount, coins, 0);
+        
+    }
+    int change(int amount, vector<int>& coins, int i) {
+        if(amount == 0)
+            return 1;
+        
+        if(amount < 0 || i >= coins.size())
+            return 0;
+        
+        return change(amount-coins[i], coins, i) + change(amount, coins, i+1);
+    }
+};
+
+
+//DP
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        int dp[n][amount+1];
+        
+        for(int i=0; i<n; i++)
+            dp[i][0] = 1;
+        
+        for(int i=0; i<n; i++)
+            for(int j=1; j<=amount; j++){
+                dp[i][j] = 0;
+                if(i > 0)
+                    dp[i][j] += dp[i-1][j];
+                if(coins[i] <= j)
+                    dp[i][j] += dp[i][j-coins[i]];
+            }
+        
+        return dp[n-1][amount];
+    }
+};
